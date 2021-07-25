8dc30c8df759b4390563412c31b06502f4827356
Eric Lambert <Eric.Jonathon.Lambert@gmail.com>
2021-07-24 22:07:15 -0700

    WIP-Basic-Settings

--- Sword Game/Assets/Scripts/Main Menu/SettingsMenu.cs
+++ Sword Game/Assets/Scripts/Main Menu/SettingsMenu.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private Dropdown resolutionDropdown;
+
+    private Resolution[] availableResolutions;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        availableResolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> resolutions = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        foreach(Resolution resolution in availableResolutions)
+        {
+            resolutions.Add(resolution.width + " x " + resolution.height);
+
+            if (resolution.width == Screen.currentResolution.width && resolution.height == Screen.currentResolution.height)
+            {
+                //currentResolutionIndex = availableResolutions.GetValue()
+            }
+        }
+
+        resolutionDropdown.AddOptions(resolutions);
     }
 
-    // Update is called once per frame
-    void Update()
+   public void SetVolume(float volume)
     {
-        
+        mixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
