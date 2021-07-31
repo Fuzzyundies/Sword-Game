ba3c15305907084df42dfacce6083d64b2d433d2
Eric Lambert <Eric.Jonathon.Lambert@gmail.com>
2021-07-30 22:17:39 -0700

    WIP-Random-Generator

--- Sword Game/Assets/Scripts/Game/GenerateLevel.cs
+++ Sword Game/Assets/Scripts/Game/GenerateLevel.cs
@@ -1,10 +1,21 @@
-using System;
+//using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    [SerializeField] private TileFactory tileFactory;
+
+    [SerializeField] private int rows;
+    [SerializeField] private int columns;
+
+    [SerializeField] private int chanceForEmpty;
+    [SerializeField] private int chanceForRaised;
+
+    [SerializeField] private double emptyTileCheck;
+    [SerializeField] private double raisedTileCheck;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -13,6 +24,28 @@ public class LevelGenerator : MonoBehaviour
 
     public void GenerateLevel()
     {
-        TileFactory.
+        double rolledValue;
+        CreateCheckRanges();
+        for (int i = 0; i < rows + columns; i++)
+        {
+            rolledValue = Random.value;
+        }
     }
+
+    private double convertIntoToPerct(int n)
+    {
+        return System.Math.Abs((double)(n % 100 / 100));
+    }
+
+    private void CreateCheckRanges()
+    {
+        if(emptyTileCheck + raisedTileCheck < 1.0)
+        {
+            //Do something clever
+            //Reduce by half
+            //Recurssive call
+        }
+        ;
+    }
+
 }
